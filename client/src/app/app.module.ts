a93d04bdbb386a371c043050d5160d83321b184b
Gabriel Kim <gabrielkim13@gmail.com>
2020-10-11 11:24:42 -0300

    Add Account Service to client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+  loggedIn: boolean = false;
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      this.loggedIn = true;
+
+      console.log(response);
+    },
+    error => console.log(error));
+  }
+}

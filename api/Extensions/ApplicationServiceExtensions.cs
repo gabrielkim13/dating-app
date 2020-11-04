b926da535827cf537d501123b9b767b0749227bd
Gabriel Kim <gabrielkim13@gmail.com>
2020-11-03 22:42:42 -0300

    Parse Postgres connection string according to env

--- api/Extensions/ApplicationServiceExtensions.cs
+++ api/Extensions/ApplicationServiceExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using api.Data;
 using api.Helpers;
 using api.Interfaces;
@@ -27,7 +28,40 @@ namespace api.Extensions
 
       services.AddDbContext<DataContext>(options =>
       {
-        options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        string connStr;
+
+        // Depending on if in development or production, use either Heroku-provided
+        // connection string, or development connection string from env var.
+        if (env == "Development")
+        {
+          // Use connection string from file.
+          connStr = config.GetConnectionString("DefaultConnection");
+        }
+        else
+        {
+          // Use connection string provided at runtime by Heroku.
+          var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+          // Parse connection URL to connection string for Npgsql
+          // postgres://mbnuyvccmlixkr:3d16b886ec9a028b0513446352000e7a3b84e615577133d96f04c1b9cc059239@ec2-52-73-199-211.compute-1.amazonaws.com:5432/d5f08c3paac23
+          connUrl = connUrl.Replace("postgres://", string.Empty);
+          var pgUserPass = connUrl.Split("@")[0];
+          var pgHostPortDb = connUrl.Split("@")[1];
+          var pgHostPort = pgHostPortDb.Split("/")[0];
+          var pgDb = pgHostPortDb.Split("/")[1];
+          var pgUser = pgUserPass.Split(":")[0];
+          var pgPass = pgUserPass.Split(":")[1];
+          var pgHost = pgHostPort.Split(":")[0];
+          var pgPort = pgHostPort.Split(":")[1];
+
+          connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+        }
+
+        // Whether the connection string came from the local development configuration file
+        // or from the environment variable from Heroku, use it to set up your DbContext.
+        options.UseNpgsql(connStr);
       });
 
       return services;
